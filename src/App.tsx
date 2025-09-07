c773ee770f5d50f3f14c7360a09af89c0900af77
Nimus-oes <sseo.code@gmail.com>
2025-09-07 15:04:42 +0900

    Implement basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <header>Header</header>
+      <Outlet />
+    </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Videos from "./pages/Videos.tsx";
+import VideoDetail from "./pages/VideoDetail.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: Videos,
+      },
+      {
+        path: "/videos",
+        Component: Videos,
+      },
+      {
+        path: "/videos/:keyword",
+        Component: Videos,
+      },
+      {
+        path: "/videos/watch/:videoId",
+        Component: VideoDetail,
+      },
+      {
+        path: "*",
+        Component: NotFound,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
