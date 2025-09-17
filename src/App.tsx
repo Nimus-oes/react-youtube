6da0f3e56d01632627fd0bcca09404a2ab816d95
Nimus-oes <sseo.code@gmail.com>
2025-09-17 14:21:00 +0900

    Set up TanStack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router";
 import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
